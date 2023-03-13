5b1c59e398cddfa69f97a78bf2ee4b7b318d3cbc
icebishop@gmail.com <icebishop@gmail.com>
2023-03-12 19:55:53 -0500

    clean code

--- M3UCheck/src/org/ice/media/m3u/app/M3UCheckApp.java
+++ M3UCheck/src/org/ice/media/m3u/app/M3UCheckApp.java
@@ -20,7 +20,7 @@ public class M3UCheckApp {
 		ProgressBar pb = null;
 
 		List<MediaFile> mediaFiles = null;
-		List<MediaFile> errorMediaFiles = new ArrayList<MediaFile>();
+		List<MediaFile> errorMediaFiles = new ArrayList<>();
 
 		String listPath = args[0];
 
@@ -30,63 +30,58 @@ public class M3UCheckApp {
 			M3UReader m3uReader = new M3UReader();
 			M3UList m3uList = m3uReader.readList(listPath);
 
-			ProgressBarBuilder pbb = new ProgressBarBuilder()
-				    .setInitialMax(m3uList.getMediaFiles().size())
-				    .setStyle(ProgressBarStyle.UNICODE_BLOCK)
-				    .setTaskName("List Check")
-				    //.setUnit(<unit name>, <unit size>)
-				    //.setUpdateIntervalMillis(<update interval>)
-				    .setMaxRenderedLength(10)
-				    .showSpeed();
-			
+			ProgressBarBuilder pbb = new ProgressBarBuilder().setInitialMax(m3uList.getMediaFiles().size())
+					.setStyle(ProgressBarStyle.UNICODE_BLOCK).setTaskName("List Check")
+					.setMaxRenderedLength(10).showSpeed();
+
 			pb = pbb.build();
-			//pb.start();
+			
 			mediaFiles = m3uList.getMediaFiles();
 			long megabytes = 0;
 			for (Iterator<MediaFile> iterator = mediaFiles.iterator(); iterator.hasNext();) {
-				MediaFile mediaFile = (MediaFile) iterator.next();
+				MediaFile mediaFile = iterator.next();
 				f = new File(mediaFile.getUrl());
 				if (!f.exists()) {
 					errorMediaFiles.add(mediaFile);
-				}
-				else {
-					megabytes = megabytes + (f.length()/1024/1024);
+				} else {
+					megabytes = megabytes + (f.length() / 1024 / 1024);
 				}
 				pb.step();
 			}
 			pb.pause();
-			
-			if(errorMediaFiles.isEmpty()) {
-				System.out.println(String.format("List %s OK Size %s MB", m3uList.getName(), megabytes));
-			}else {
-				System.out.println(String.format("%s Errors on list %s:",errorMediaFiles.size(), m3uList.getName()));
+
+			if (errorMediaFiles.isEmpty()) {
+				System.console().writer().println(String.format("List %s OK Size %s MB", m3uList.getName(), megabytes));
+			} else {
+				System.console().writer().println(String.format("%s Errors on list %s:", errorMediaFiles.size(), m3uList.getName()));
 				for (Iterator<MediaFile> iterator = errorMediaFiles.iterator(); iterator.hasNext();) {
-					MediaFile mediaFile = (MediaFile) iterator.next();
-					System.out.println(String.format("%s : %s " , mediaFile.getPosition(), mediaFile.getUrl()));
+					MediaFile mediaFile = iterator.next();
+					System.console().writer().println(String.format("%s : %s ", mediaFile.getPosition(), mediaFile.getUrl()));
 				}
-				 //Scanner in = new Scanner(System.in);
-				System.out.println("Repair list? (Y/N)" );
-				String repair =  System.console().readLine();
-				System.out.println(repair);
+				System.console().writer().println("Repair list? (Y/N)");
+				String repair = System.console().readLine();
+				System.console().writer().println(repair);
 				String newpath = "";
-				if(repair.equals("Y")) {
+				if (repair.equals("Y")) {
 					for (Iterator<MediaFile> iterator = errorMediaFiles.iterator(); iterator.hasNext();) {
-						MediaFile mediaFile = (MediaFile) iterator.next();
-						System.out.println(String.format("Set path for %s : %s " , mediaFile.getPosition(), mediaFile.getUrl()));
+						MediaFile mediaFile = iterator.next();
+						System.console().writer().println(
+								String.format("Set path for %s : %s ", mediaFile.getPosition(), mediaFile.getUrl()));
 						newpath = System.console().readLine();
-						System.out.println(newpath);
+						System.console().writer().println(newpath);
 						mediaFile.setUrl(newpath);
 						m3uList.getMediaFilesMap().put(mediaFile.getPosition(), mediaFile);
 					}
 					M3UWriter.writeList(true, true, File.separator, listPath, m3uList.getMediaFiles());
 				}
-				// in.close();
-			}
 				
+			}
+
 		} catch (Exception e) {
 			e.printStackTrace();
 		} finally {
-			pb.close();
+			if (pb != null)
+				pb.close();
 		}
 
 	}
